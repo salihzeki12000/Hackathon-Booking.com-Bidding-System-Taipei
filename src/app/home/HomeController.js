a09715b2c30d4389b9db253398608e85b16437ca
ed <ultralabed@gmail.com>
2017-03-11 17:59:04 +0800

    style(home): Add style for home page.

--- src/app/home/HomeController.js
+++ src/app/home/HomeController.js
@@ -16,10 +16,32 @@ export default class HomeController {
     this.location = '';
     this.days = 1;
     this.budget = null;
+    this.services = {
+      meeting_room: false,
+      casino: false,
+      free_parking: false,
+      pickup: false,
+      laundry: false,
+      fitness: false,
+      garden: false,
+      swimming_pool: false,
+      hot_spring_bath: false,
+      restaurant: false,
+    };
+    this.servicesSelected = [];
   }
 
   search() {
-    console.log(`data${this.days}`);
+    console.log(`data${this.days}, and data array${this.servicesSelected}`);
     this.$state.go('cbid');
+  }
+
+  selectService(serviceKey) {
+    let findService = (item) => item === serviceKey;
+    let serviceIndex = this.servicesSelected.findIndex(findService);
+    serviceIndex >= 0 ?
+      this.servicesSelected.splice(serviceIndex, 1)
+      :
+      this.servicesSelected.push(serviceKey);
   }
 }
