9b285d6bc44e3248438dafdddde347a178a8116b
ed <ultralabed@gmail.com>
2017-03-11 17:12:52 +0800

    Refactor(Remove) Remvoe unused files.

--- src/common/dialog/freeTrialDialogController.js
+++
@@ -1,9 +0,0 @@
-export default class FreeTrialDialogController {
-
-  /*@ngInject*/
-  constructor($scope) {
-    this.$scope = $scope;
-    this.dialogData = this.$scope.ngDialogData;
-    this.query_string = this.dialogData.query_string;
-  }
-}
